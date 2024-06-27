f4f0c9cb89b910702da195c77747179d12412761
subratsamal <samalsubrat919@gmail.com>
2024-06-27 13:17:57 +0530

    responsive

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -48,8 +48,8 @@ const DesignConfigurator = ({
   });
 
   return (
-    <div className="relative mt-20 grid grid-cols-3 mb-20 pb-20">
-      <div className="relative h-[37.5rem] overflow-hidden col-span-2 w-full max-w-4xl flex items-center justify-center rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2">
+    <div className="relative my-20 pb-20 grid grid-cols-1 lg:grid-cols-5">
+      <div className="relative h-[37.5rem] overflow-hidden col-span-3 w-full max-w-4xl flex items-center justify-center rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2">
         <div className="relative w-60 bg-opacity-40 pointer-events-none aspect-[896/1831]">
           <AspectRatio
             ratio={896 / 1831}
@@ -97,8 +97,8 @@ const DesignConfigurator = ({
         </Rnd>
       </div>
 
-      <div className="h-[37.5rem] flex flex-col bg-white">
-        <h2 className="tracking-tight font-bold text-3xl px-9">
+      <div className="h-[37.5rem] w-full lg:pl-10 col-span-full lg:col-span-2 flex flex-col bg-white">
+        <h2 className="tracking-tight font-bold text-3xl px-9 max-lg:mt-3">
           Customize your case.
         </h2>
         <div className="w-full px-8 h-1 bg-white my-3">
