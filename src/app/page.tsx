d1fb68442f5634501e6f24023c28c8a4d8b87cc0
subratsamal <samalsubrat919@gmail.com>
2024-07-11 19:03:54 +0530

    scroll fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -96,7 +96,7 @@ export default function Home() {
             <div className="relative md:max-w-xl">
               <img
                 src="/your-image.png"
-                className="absolute w-40 lg:w-52 left-56 -top-20 select-none hidden sm:block lg:hidden xl:block"
+                className="absolute w-40 lg:w-52 left-52 -top-20  select-none hidden sm:block lg:hidden xl:block"
               />
               <img
                 src="/line.png"
