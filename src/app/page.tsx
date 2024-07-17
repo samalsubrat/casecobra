7d9b2ab27729225327c11b5d7c1b940cc0ba9714
Subrat Samal <samalsubrat919@gmail.com>
2024-07-17 16:32:28 +0530

    added max width wrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import Hero from "@/components/Landing Page/Hero";
 import ValueProps from "@/components/Landing Page/ValueProps";
 import Last from "@/components/Landing Page/Last";
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 
 export default function Home() {
   return (
     <div className="bg-slate-50 grainy-light">
-      <Hero />
+      <MaxWidthWrapper>
+        <Hero />
+      </MaxWidthWrapper>
       <ValueProps />
       <Last />
     </div>
